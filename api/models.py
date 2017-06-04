edc54771af483d7595660b35340a886de0af44cb
Arleigh Atkinson <arleigh.atkinson@nikolamotor.com>
2017-06-04 17:04:37 -0600

    Add item model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+	"""Item model."""
+	name = models.CharField(max_length=255, blank=False)
+	completed = models.BooleanField()
+	date_created = models.DateTimeField(auto_now_add=True)
+	date_modified = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		"""Format model name and completed"""
+		return "{}".format(self.name, self.completed)
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+	"""Test suite for item model."""
+
+	def setUp(self):
+		self.item_name = 'Clean up kitchen.'
+		self.item = Item(name=self.item_name, completed=False)
+
+	def test_model_can_create_an_item(self):
+		count = Item.objects.count()
+		self.item.save()
+		updated_count = Item.objects.count()
+		self.assertEqual((count + 1), updated_count)
