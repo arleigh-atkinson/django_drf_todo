2a3d7118aab6760fadef9a9430ddde0a2954f9a4
Arleigh Atkinson <arleigh.atkinson@nikolamotor.com>
2017-06-04 17:39:56 -0600

    Add GET endpoint for items

--- api/tests.py
+++ api/tests.py
@@ -30,3 +30,11 @@ class ViewTestCase(TestCase):
 
 	def test_api_can_create_an_item(self):
 		self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+	def test_api_can_get_an_item(self):
+		item = Item.objects.get()
+		response = self.client.get(
+			reverse('details', kwargs={'pk': item.id}),
+			format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertContains(response, item)
--- api/views.py
+++ api/views.py
@@ -4,8 +4,16 @@ from .serializers import ItemSerializer
 from .models import Item
 
 class CreateView(generics.ListCreateAPIView):
+	"""Handles HTTP POST requests"""
+	
 	queryset = Item.objects.all()
 	serializer_class = ItemSerializer
 
 	def perform_create(self, serializer):
 		serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+	"""Handles HTTP GET, PUT, and DELETE requests"""
+
+	queryset = Item.objects.all()
+	serializer_class = ItemSerializer
