f1a58ffaff33bc70f095367320c97b8a3342a514
Otis Stamp <otistamp@gmail.com>
2017-06-05 10:06:29 -0400

    CLN: dockerization and reorganization

--- api/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-from .models import Item
-
-class IsUser(BasePermission):
-	def has_object_permissions(self, request, view, obj):
-		if isinstance(obj, Item):
-			return obj.user == request.user
-		return obj.user == request.user
--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/arleighatkinson/Documents/django_projects/todo_api/env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
